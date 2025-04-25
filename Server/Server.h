ff4e976673bab181a27622eb5f93806245443990
amalya.fatullaeva97@gmail.com <amalya.fatullaeva97@gmail.com>
2025-04-26 02:23:46 +0300

    Добавлены комментарии для методов в Server.h

--- Server/Server.h
+++ Server/Server.h
@@ -40,23 +40,23 @@ private:
 public:
     Server();
     ~Server();
-    int init();
-    int init_socket();
-    int init_DB();
-    void Write(std::string text);
-    std::string Read();
-    void exit();
-    void INSERT_Users(Users us);
-    void INSERT_prvt_message(Message msg);
-    void INSERT_pblc_message(Message msg);
-    int Select_Users_DB_status(const std::wstring& request);
-    void Delete_prvt_msg_DB(int id);
-    void Delete_pblc_msg_DB(int id);
-    int get_ID_DB(std::string login);
-    void get_Users_DB();
-    void get_Users_pswd_DB();
-    void get_private_message_DB(std::string login);
-    void get_public_message_DB(std::string login);
-    int get_Users_DB_status();
-    void close_DB();
+    int init(); // инициализация сервера
+    int init_socket(); // инициализация сокета
+    int init_DB(); // инициализация БД
+    void Write(std::string text); // отправка данных клиенту
+    std::string Read(); // чтение данных от клиента
+    void exit(); // закрытие сокета
+    void INSERT_Users(Users us); // запись данных в таблицу Users/ Users_pswd
+    void INSERT_prvt_message(Message msg); // запись данных в таблицу private_message
+    void INSERT_pblc_message(Message msg); // запись данных в таблицу public_message
+    int Select_Users_DB_status(const std::wstring& request); // выборка для метода get_Users_DB_status()
+    void Delete_prvt_msg_DB(int id); // удаление уже прочитанных сообщений из таблицы private_message
+    void Delete_pblc_msg_DB(int id); // удаление уже прочитанных сообщений из таблицы public_message
+    int get_ID_DB(std::string login); // получение id по логину
+    void get_Users_DB(); // Метод выводит данные из таблицы Users
+    void get_Users_pswd_DB(); // Метод выводит данные из таблицы Users_pswd
+    void get_private_message_DB(std::string login); // Метод выводит данные из таблицы private_message
+    void get_public_message_DB(std::string login); // Метод выводит данные из таблицы public_message
+    int get_Users_DB_status(); // если есть строки в таблице Users, то возвращает 1, иначе -1
+    void close_DB(); // закрытие БД
 };
